96ccbf5e0c6f45c186c1e94842858b9ba5d3cc69
Krishna-kumar13 <125043869+Krishna-kumar13@users.noreply.github.com>
2023-02-10 21:54:14 +0530

    Update streamlit_app.py
    added break menu header and 3 iterms

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Onega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-
